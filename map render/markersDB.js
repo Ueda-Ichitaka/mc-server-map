d6fb13946e9b9ce85257372232c14b8dd20ca8e9
Ueda-Ichitaka <ueda.ichitaka@gmx.de>
2015-10-20 17:30:52 +0200

    base 1

---
+++ map render/markersDB.js
@@ -0,0 +1,160 @@
+var markersDB={
+  "Mob_Farms7dc7_a951": {
+    "raw": [
+      {
+        "y": 75, 
+        "x": -70, 
+        "hovertext": "Monsterfalle\n", 
+        "z": -167, 
+        "text": "Monsterfalle\n"
+      }
+    ], 
+    "name": "Mob Farms", 
+    "created": false
+  }, 
+  "Farms7dd7_a951": {
+    "raw": [
+      {
+        "y": 67, 
+        "x": 79, 
+        "hovertext": "Farm Site\n", 
+        "z": 40, 
+        "text": "Farm Site\n"
+      }
+    ], 
+    "name": "Farms", 
+    "created": false
+  }, 
+  "Dungeons7dcf_a951": {
+    "raw": [
+      {
+        "y": 62, 
+        "x": -604, 
+        "hovertext": "End Festung\n", 
+        "z": -157, 
+        "text": "End Festung\n"
+      }, 
+      {
+        "y": 63, 
+        "x": 1143, 
+        "hovertext": "Atlantis 1\n", 
+        "z": 1879, 
+        "text": "Atlantis 1\n"
+      }, 
+      {
+        "y": 63, 
+        "x": 1576, 
+        "hovertext": "Atlantis 2\n", 
+        "z": 1223, 
+        "text": "Atlantis 2\n"
+      }, 
+      {
+        "y": 70, 
+        "x": -111, 
+        "hovertext": "Eingang Mine\n", 
+        "z": -160, 
+        "text": "Eingang Mine\n"
+      }
+    ], 
+    "name": "Dungeons", 
+    "created": false
+  }, 
+  "Factories7ddf_a951": {
+    "raw": [], 
+    "name": "Factories", 
+    "created": false
+  }, 
+  "Townsf587_a951": {
+    "raw": [
+      {
+        "y": 77, 
+        "x": 44, 
+        "hovertext": "Dorf\nSpawn, Base of Ueda Ichitaka", 
+        "z": 18, 
+        "text": "Dorf\nSpawn, Base of Ueda Ichitaka"
+      }, 
+      {
+        "y": 72, 
+        "x": 1645, 
+        "hovertext": "Bauernhaus\n", 
+        "z": 135, 
+        "text": "Bauernhaus\n"
+      }, 
+      {
+        "y": 64, 
+        "x": 811, 
+        "hovertext": "Strandhaus\n", 
+        "z": 926, 
+        "text": "Strandhaus\n"
+      }
+    ], 
+    "name": "Towns", 
+    "created": false
+  }, 
+  "Minesf54b_a951": {
+    "raw": [], 
+    "name": "Mines", 
+    "created": false
+  }, 
+  "Castles7ddb_a951": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": 254, 
+        "hovertext": "Festung Ueda\n", 
+        "z": -1510, 
+        "text": "Festung Ueda\n"
+      }, 
+      {
+        "y": 70, 
+        "x": 270, 
+        "hovertext": "Festung Chaos\n", 
+        "z": 170, 
+        "text": "Festung Chaos\n"
+      }
+    ], 
+    "name": "Castles", 
+    "created": false
+  }, 
+  "Construction_Sites7dcb_a951": {
+    "raw": [
+      {
+        "y": 38, 
+        "x": -141, 
+        "hovertext": "Cavespider Spawner\n", 
+        "z": -207, 
+        "text": "Cavespider Spawner\n"
+      }, 
+      {
+        "y": 71, 
+        "x": -19, 
+        "hovertext": "Spider Dungeon\n", 
+        "z": -189, 
+        "text": "Spider Dungeon\n"
+      }, 
+      {
+        "y": 69, 
+        "x": -205, 
+        "hovertext": "Hexenhaus\n", 
+        "z": -1499, 
+        "text": "Hexenhaus\n"
+      }, 
+      {
+        "y": 94, 
+        "x": 128, 
+        "hovertext": "Big Tower Chaos\n", 
+        "z": 118, 
+        "text": "Big Tower Chaos\n"
+      }, 
+      {
+        "y": 75, 
+        "x": 190, 
+        "hovertext": "Innenhof Chaos\n", 
+        "z": 163, 
+        "text": "Innenhof Chaos\n"
+      }
+    ], 
+    "name": "Construction Sites", 
+    "created": false
+  }
+};
